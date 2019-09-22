520205f3b5068e446f33707b65323d1d8c1aa042
themansouriahs <ahmeds.mansouri@gmail.com>
2019-09-22 20:05:23 +0100

    heatmap done

--- src/heat_map.c
+++ src/heat_map.c
@@ -13,6 +13,34 @@ static int	is_full(int **hm, int h, int w)
 	return (1);
 }
 
+static void	fill_hm(t_board *br, int x, int y, int i)
+{
+	if (br->hm[y][x] == i && x + 1 < br->w && y + 1 < br->h &&
+		br->hm[y + 1][x + 1] != i && br->hm[y + 1][x + 1] == 0)
+		br->hm[y + 1][x + 1] = i + 1;
+	if (br->hm[y][x] == i && x + 1 < br->w && y - 1 > 0 &&
+		br->hm[y - 1][x + 1] != i && br->hm[y - 1][x + 1] == 0)
+		br->hm[y - 1][x + 1] = i + 1;
+	if (br->hm[y][x] == i && x - 1 >= 0 && y - 1 >= 0 &&
+		br->hm[y - 1][x - 1] != i && br->hm[y - 1][x - 1] == 0)
+		br->hm[y - 1][x - 1] = i + 1;
+	if (br->hm[y][x] == i && x - 1 >= 0 && y + 1 < br->h &&
+		br->hm[y + 1][x - 1] != i && br->hm[y + 1][x - 1] == 0)
+		br->hm[y + 1][x - 1] = i + 1;
+	if (br->hm[y][x] == i && x + 1 < br->w &&
+		br->hm[y][x + 1] != i && br->hm[y][x + 1] == 0)
+		br->hm[y][x + 1] = i + 1;
+	if (br->hm[y][x] == i && x - 1 >= 0 &&
+		br->hm[y][x - 1] != i && br->hm[y][x - 1] == 0)
+		br->hm[y][x - 1] = i + 1;
+	if (br->hm[y][x] == i && y + 1 < br->h &&
+		br->hm[y + 1][x] != i && br->hm[y + 1][x] == 0)
+		br->hm[y + 1][x] = i + 1;
+	if (br->hm[y][x] == i && y - 1 >= 0 &&
+		br->hm[y - 1][x] != i && br->hm[y - 1][x] == 0)
+		br->hm[y - 1][x] = i + 1;
+}
+
 void		heatmap(t_board *br)
 {
 	int i;
@@ -22,34 +50,9 @@ void		heatmap(t_board *br)
 	i = 1;
 	while (!is_full(br->hm, br->h, br->w) && (y = -1))
 	{
-		// if (is_full(br->hm, br->h, br->w))
-		// 	break ;
-		// y = 0;
 		while (++y < br->h && (x = -1))
-		{
-			// x = 0;
 			while (++x < br->w)
-			{
-				if (br->hm[y][x] == i && x + 1 < br->w && y + 1 < br->h && br->hm[y + 1][x + 1] != i && br->hm[y + 1][x + 1] == 0)
-					br->hm[y + 1][x + 1] = i + 1;
-				if (br->hm[y][x] == i && x + 1 < br->w && y - 1 > 0 && br->hm[y - 1][x + 1] != i && br->hm[y - 1][x + 1] == 0)
-					br->hm[y - 1][x + 1] = i + 1;
-				if (br->hm[y][x] == i && x - 1 >= 0 && y - 1 >= 0 && br->hm[y - 1][x - 1] != i && br->hm[y - 1][x - 1] == 0)
-					br->hm[y - 1][x - 1] = i + 1;
-				if (br->hm[y][x] == i && x - 1 >= 0 && y + 1 < br->h && br->hm[y + 1][x - 1] != i && br->hm[y + 1][x - 1] == 0)
-					br->hm[y + 1][x - 1] = i + 1;
-				if (br->hm[y][x] == i && x + 1 < br->w && br->hm[y][x + 1] != i && br->hm[y][x + 1] == 0)
-					br->hm[y][x + 1] = i + 1;
-				if (br->hm[y][x] == i && x - 1 >= 0 && br->hm[y][x - 1] != i && br->hm[y][x - 1] == 0)
-					br->hm[y][x - 1] = i + 1;
-				if (br->hm[y][x] == i && y + 1 < br->h && br->hm[y + 1][x] != i && br->hm[y + 1][x] == 0)
-					br->hm[y + 1][x] = i + 1;
-				if (br->hm[y][x] == i && y - 1 >= 0 && br->hm[y - 1][x] != i && br->hm[y - 1][x] == 0)
-					br->hm[y - 1][x] = i + 1;
-				// x++;
-			}
-			// y++;
-		}	
+				fill_hm(br, x, y, i);
 		i++;
 	}
 }
