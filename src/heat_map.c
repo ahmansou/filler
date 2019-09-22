7e7f97a1629b701ad762f3ce9a8e38f119c7fb50
themansouriahs <ahmeds.mansouri@gmail.com>
2019-09-22 19:56:26 +0100

    heatmap done

--- src/heat_map.c
+++ src/heat_map.c
@@ -20,16 +20,15 @@ void		heatmap(t_board *br)
 	int y;
 
 	i = 1;
-	// while (i < 8)
-	while (1)
+	while (!is_full(br->hm, br->h, br->w) && (y = -1))
 	{
-		if (is_full(br->hm, br->h, br->w))
-			break ;
-		y = 0;
-		while (y < br->h)
+		// if (is_full(br->hm, br->h, br->w))
+		// 	break ;
+		// y = 0;
+		while (++y < br->h && (x = -1))
 		{
-			x = 0;
-			while (x < br->w)
+			// x = 0;
+			while (++x < br->w)
 			{
 				if (br->hm[y][x] == i && x + 1 < br->w && y + 1 < br->h && br->hm[y + 1][x + 1] != i && br->hm[y + 1][x + 1] == 0)
 					br->hm[y + 1][x + 1] = i + 1;
@@ -47,9 +46,9 @@ void		heatmap(t_board *br)
 					br->hm[y + 1][x] = i + 1;
 				if (br->hm[y][x] == i && y - 1 >= 0 && br->hm[y - 1][x] != i && br->hm[y - 1][x] == 0)
 					br->hm[y - 1][x] = i + 1;
-				x++;
+				// x++;
 			}
-			y++;
+			// y++;
 		}	
 		i++;
 	}
