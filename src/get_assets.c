bfb42938abb95625288a1d2cf56cab330a32246c
Ahmed-saad Mansouri <ahmansou@e1r5p8.1337.ma>
2019-10-26 17:02:56 +0100

    done

--- src/get_assets.c
+++ src/get_assets.c
@@ -12,29 +12,28 @@
 
 #include "filler.h"
 
-static void	get_board(t_board *board, char **line)
+static void	get_board(t_board *board, char *line)
 {
 	char	*tmp;
 	int		i;
 
-	tmp = *line + 8;
-	ft_strdel(line);
-	// free(*line);
+	tmp = line + 8;
+	ft_strdel(&line);
 	board->h = ft_atoi(tmp);
 	while (*tmp >= '0' && *tmp <= '9')
 		tmp++;
 	board->w = ft_atoi(tmp + 1);
 	board->m = (char**)malloc(sizeof(char*) * (board->h + 1));
 	i = 0;
-	if (get_next_line(0, line) != 1)
+	if (get_next_line(0, &line) != 1)
 		return ;
-	ft_strdel(line);
+	ft_strdel(&line);
 	while (i < board->h)
 	{
-		if (get_next_line(0, line) != 1)
+		if (get_next_line(0, &line) != 1)
 			return ;
-		board->m[i] = ft_strdup(*line + 4);
-		ft_strdel(line);
+		board->m[i] = ft_strdup(line + 4);
+		ft_strdel(&line);
 		i++;
 	}
 	board->m[i] = 0;
@@ -80,7 +79,10 @@ int		get_assets(t_board *board, t_piece *pc)
 	while (get_next_line(0, &line) > 0)
 	{
 		if (!ft_strncmp(line, "Plateau", 7))
-			get_board(board, &line);
+		{
+			get_board(board, ft_strsub(line, 0, ft_strlen(line)));
+			ft_strdel(&line);
+		}
 		else if (!ft_strncmp(line, "Piece", 5))
 		{
 			get_piece(pc, line);
