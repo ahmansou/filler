6040c32dadf4ab0ad875866f2f812b4de70e4750
Ahmed-saad Mansouri <ahmansou@e1r5p8.1337.ma>
2019-10-26 17:08:36 +0100

    done

--- src/get_assets.c
+++ src/get_assets.c
@@ -15,25 +15,27 @@
 static void	get_board(t_board *board, char *line)
 {
 	char	*tmp;
+	char 	*l;
 	int		i;
 
-	tmp = line + 8;
-	free(line);
+	l = ft_strdup(line);
+	tmp = l + 8;
+	free(l);
 	board->h = ft_atoi(tmp);
 	while (*tmp >= '0' && *tmp <= '9')
 		tmp++;
 	board->w = ft_atoi(tmp + 1);
 	board->m = (char**)malloc(sizeof(char*) * (board->h + 1));
 	i = 0;
-	if (get_next_line(0, &line) != 1)
+	if (get_next_line(0, &l) != 1)
 		return ;
-	ft_strdel(&line);
+	ft_strdel(&l);
 	while (i < board->h)
 	{
-		if (get_next_line(0, &line) != 1)
+		if (get_next_line(0, &l) != 1)
 			return ;
-		board->m[i] = ft_strdup(line + 4);
-		ft_strdel(&line);
+		board->m[i] = ft_strdup(l + 4);
+		ft_strdel(&l);
 		i++;
 	}
 	board->m[i] = 0;
@@ -80,7 +82,7 @@ int		get_assets(t_board *board, t_piece *pc)
 	{
 		if (!ft_strncmp(line, "Plateau", 7))
 		{
-			get_board(board, ft_strsub(line, 0, ft_strlen(line)));
+			get_board(board, line);
 			ft_strdel(&line);
 		}
 		else if (!ft_strncmp(line, "Piece", 5))
